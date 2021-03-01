d52ead416c752157a83b2637385df0222a0a448b
Nurlan K <nurlan.wap3@gmail.com>
2021-03-01 14:26:17 +0400

    refactor: use join

--- src/components/blocks/MovieCard.tsx
+++ src/components/blocks/MovieCard.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Movie } from '../../assets/js/types'
 
 const Search: React.FC<Movie> = ({ id, title, year, genres, posterurl }) => {
-    
-    const listGenres = genres.map((item: string, i) => {
-        const end = (genres.length - 1) == i
-        return end ? item : `${item} & `
-    })
+
     return (
         <div className='movie-card'>
             <img src={posterurl} alt={title} width='320' height='450' />
@@ -14,7 +10,7 @@ const Search: React.FC<Movie> = ({ id, title, year, genres, posterurl }) => {
                 <span className='title'>{ title }</span>
                 <span className='year'>{ year }</span>
             </div>
-            <div className='genres'>{ listGenres }</div>
+            <div className='genres'>{ genres.join(' & ') }</div>
         </div>
     );
 }
