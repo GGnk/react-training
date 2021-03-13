29fc2302494714ce48024b6c7181bf0299af2022
Nurlan K <nurlan.wap3@gmail.com>
2021-03-13 21:18:18 +0400

    feat: add task 5

---
+++ src/assets/js/hooks.ts
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { Movie } from './types'
+
+export const useMovieDetails = (): [boolean, Movie | null, React.Dispatch<React.SetStateAction<Movie | null>>] => {
+    const [showMovieDetails, setShowMovieDetails] = useState<boolean>(false);
+    const [movie, setMovie] = useState<Movie | null>(null);
+
+    const cacheSetMovie = useCallback((film) => setMovie(film), [movie])
+    
+    useEffect((): void => {
+        movie ? setShowMovieDetails(true) : setShowMovieDetails(false)
+    }, [movie])
+
+    return [showMovieDetails, movie, cacheSetMovie]
+}
